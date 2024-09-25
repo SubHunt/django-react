fab51d00fc2d24d2097ac7c59e2c2d8cd0bde156
SubHunt <phaeton.kz@gmail.com>
2024-09-25 22:33:52 +0300

    2_axios_api

--- 1_creat_project/firstproject/firstproject/urls.py
+++ 1_creat_project/firstproject/firstproject/urls.py
@@ -15,12 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 
 from myapp.views import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', index)
+    path('', index),
+    path('api/', include('myapp.api.urls'))
 ]
